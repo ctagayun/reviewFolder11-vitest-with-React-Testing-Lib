2b80157c54203a2dab94c076a3ea82e5244dce5e
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-18 16:24:55 -0500

    save

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-//import App from './App';
+import App from './App';
 
 describe('something truthy and falsy', () => {
   it('true to be true', () => {
@@ -12,12 +12,12 @@ describe('something truthy and falsy', () => {
   });
 });
 
-// describe('App', () => {
-//     it('renders headline', () => {
-//       render(<App title="React" />);
+describe('App', () => {
+    it('renders headline', () => {
+      render(<App title="React" />);
   
-//       screen.debug();
+      screen.debug();
   
-//       // check if App components renders headline
-//     });
-//   });
+      // check if App components renders headline
+    });
+  });
